d60181f315704b44ed8fbc594627220cd9dc4a27
yasmimfeltz <vilenyasmimkalfeltz@gmail.com>
2021-01-07 15:12:20 -0300

    formatting values and lower and upper

---
+++ aula1/Mdecima_terceira.py
@@ -0,0 +1,16 @@
+print('▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀NEW CLASS▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀')
+"""
+manipulating strings
+* String index # String indices
+* Fatiamento de string [inicio:fim:passo]
+* Functions built-in len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em cada tipo.
+
+links: https://docs.python.org/3/library/stdtypes.html (tipos built-in)
+https://docs.python.org/3/library/functions.html (funções built-in)
+"""
+# positive  [012345678]
+text      = 'Python s2'
+# negative  [987654321]
+new_string = text[2:6]
+print(new_string)
