ace07ae50f41db7c2df494b412331259aa9c5e7f
yasmimfeltz <vilenyasmimkalfeltz@gmail.com>
2021-01-14 09:17:29 -0300

    vigesima primeira

---
+++ aula1/Rvigesima_primeira.py
@@ -0,0 +1,8 @@
+print('▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀NEW CLASS▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀▄▀')
+# for/ else in python
+var = ['Yasmim', 'Vilen', 'Kalfeltz']
+for value in var:
+    if value.startswith('V'):
+        print('Começa com V', value)
+    else:
+        print('Não começa com V', value)
